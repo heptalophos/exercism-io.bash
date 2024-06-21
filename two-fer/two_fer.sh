b25dec9a43dd3273c05040814387a5bd0aab1821
Zero Delta <zerodeltahedge@gmail.com>
2024-06-21 12:48:09 +0300

    Reformat two-fer.sh

--- two-fer/two_fer.sh
+++ two-fer/two_fer.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 main () {
-local name=${1:-you}
-echo "One for $name, one for me."
-return 0
+    local name=${1:-you}
+    echo "One for $name, one for me."
+    return 0
 }
 
 main "$@"
