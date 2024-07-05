3fd41b6b8aee4e5e5b20e1841d8835a55d07aed3
Zero Delta <zerodeltahedge@gmail.com>
2024-07-05 14:07:43 +0300

    Refactor pangram.sh

--- pangram/pangram.sh
+++ pangram/pangram.sh
@@ -1,40 +1,17 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
-
 pangram() {
-    sentence=${1^^}
+    (( $# == 1 )) && sentence=${1^^} || exit 1
     local -A letters=()
     for i in $(seq 0 ${#1}); do
-        letr=${sentence:i:1}
-        [[ $letr == [A-Z] ]] && letters[$letr]='v' 
+        gramma=${sentence:i:1}
+        [[ $gramma =~ [A-Z] ]] && letters[$gramma]='yes' 
     done
     (( ${#letters[@]} == 26 )) && echo "true" || echo "false" 
 }
 
 main() {
-    pangram "$@" && exit 0
+    pangram "$1" && exit 0
 }
 
 set -e -u
