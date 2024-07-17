dbd74d220c26f31b154553585eaee22e8e0e94c7
Zero Delta <zerodeltahedge@gmail.com>
2024-07-17 11:12:17 +0300

    bob.sh - passing all tests

--- bob/bob.sh
+++ bob/bob.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+boolx() { (( $1 == 0 )) && echo 't' || echo 'f'; }
+silent() { [[ -z "$1" ]]; boolx $?; }
+question() { [[ "$1" == *"?" ]]; boolx $?; }
+yelling() {
+    local upper=${1^^} lower=${1,,}
+    [[ "$upper" = "$1" && "$upper" != "$lower" ]]
+    boolx $?
+}
+bob() {
+    read -d '' query <<< "$(sed 's/[\r]//gi' <<< ${1:-})"
+    local s=$(silent "$query") y=$(yelling "$query") 
+          q=$(question "$query")
+    case "${s}${y}${q}" in
+    "fft") echo "Sure." ;;
+    "ftf") echo "Whoa, chill out!" ;;
+    "ftt") echo "Calm down, I know what I'm doing!" ;;
+    "tff") echo "Fine. Be that way!" ;;
+    *) echo "Whatever." ;;
+    esac
+}
+
+main() {
+    bob "$@" && exit 0
+}
+
+set -e -u && main "$@"
