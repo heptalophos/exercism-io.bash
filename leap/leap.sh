4e5bb39346d3b9ab434919d20069848d5486ecfe
Zero Delta <zerodeltahedge@gmail.com>
2024-08-27 16:13:26 +0300

    Update leap.sh

--- leap/leap.sh
+++ leap/leap.sh
@@ -4,8 +4,7 @@ boolx() { [[ "$*" == 0 ]] && echo false || echo true; }
 errexit() { echo "$*" >&2 && exit 1; }
 
 leap() {
-    [[ "$#" -ne 1 || "$1" =~ [^0-9] ]] && 
-    errexit "Usage: "${0#*}" <year>"
+    [[ "$#" -ne 1 || "$1" =~ [^0-9] ]] && errexit "Usage: ${0#*} <year>"
     if (( "$1" % 100 == 0 )); then 
         boolx $(( "$1" % 400 == 0 ))
     else
