7d25778427d3eb31e05a20528f1618d4b90e53d5
Zero Delta <zerodeltahedge@gmail.com>
2024-09-05 23:16:32 +0300

    Refactor

--- darts/darts.sh
+++ darts/darts.sh
@@ -3,14 +3,14 @@
 errexit() { echo "$*" >&2 && exit 1; }
 
 darts() {
-    [[ $# -lt 2 || $* =~ ([[:alpha:]]) ]] && 
+    [[ ! $# -eq 2 || $* =~ [[:alpha:]] ]] && 
     errexit "Usage: ${0#*} <x> <y>"
-    local sumsq=`awk 'BEGIN {print (ARGV[1] ** 2 + ARGV[2] ** 2)}' $1 $2`
-    local radius=`awk 'BEGIN {print (ARGV[1] ** (1.0 / ARGV[2]))}' "$sumsq" 2`
+    local sums=`awk 'BEGIN { print (ARGV[1] ** 2 + ARGV[2] ** 2) }' $1 $2`
+    local eucd=`awk 'BEGIN {print (ARGV[1] ** (1.0 / ARGV[2]))}' "$sums" 2`
     local points=0
-    if (( $(echo "$radius <= 10" | bc -l) )); then (( points += 1 )); fi
-    if (( $(echo "$radius <= 5" | bc -l) )); then (( points += 4 )); fi
-    if (( $(echo "$radius <= 1" | bc -l) )); then (( points += 5 )); fi
+    if (( $(echo "$eucd <= 10" | bc -l) )); then (( points += 1 )); fi
+    if (( $(echo "$eucd <= 5"  | bc -l) )); then (( points += 4 )); fi
+    if (( $(echo "$eucd <= 1"  | bc -l) )); then (( points += 5 )); fi
     echo $points
 }
 
