8cec646ea992c2747ca2b776065ad68d1a324fab
Zero Delta <zerodeltahedge@gmail.com>
2024-09-20 01:22:14 +0300

    Create nucleotide_count.sh

--- nucleotide-count/nucleotide_count.sh
+++ nucleotide-count/nucleotide_count.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+errexit() { echo "$*" >&2 && exit 1; }
+
+counts() {
+    local strand="$1"
+    local -A counts=([A]=0 [C]=0 [G]=0 [T]=0)
+    for ((i=0; i < ${#strand}; ++i)); do
+        local nucl="${strand:$i:1}"
+        case "$nucl" in
+        A | C | G | T) ((counts[$nucl]++));;
+        *) errexit "Invalid nucleotide in strand";;
+        esac
+    done
+    for n in A C G T; do
+        echo "$n: ${counts[$n]}"
+    done
+}
+
+main() {
+    counts "$1" && exit 0
+}
+
+set -e -u && main "$@"
