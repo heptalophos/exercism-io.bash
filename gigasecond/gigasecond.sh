5918a54f93903235c91a127ea50235e5fbe25daf
Zero Delta <zerodeltahedge@gmail.com>
2024-10-01 11:48:48 +0300

    Refactor

--- gigasecond/gigasecond.sh
+++ gigasecond/gigasecond.sh
@@ -4,7 +4,7 @@ main() {
     local -i seconds
     seconds=$(TZ=UTC date -d "$1" "+%s")
     (( seconds += 10 ** 9 ))
-    echo $(TZ=UTC date --date="@$seconds" "+%FT%T") && exit 0
+    TZ="UTC" date --date="@$seconds" "+%FT%T" && exit 0
 }
 
 set -eu && main "$@"
