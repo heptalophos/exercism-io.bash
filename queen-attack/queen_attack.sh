fd487603625b332de93383998663465343823a1f
Zero Delta <zerodeltahedge@gmail.com>
2024-10-15 23:02:15 +0300

    Refactor queen-attack

--- queen-attack/queen_attack.sh
+++ queen-attack/queen_attack.sh
@@ -3,7 +3,7 @@
 errexit() { echo "$*" >&2 && exit 1; }
 
 validate_board() {
-    local -  i wrank bfile brank bfile
+    local -i wrank bfile brank bfile
     wrank="$1"; wfile="$2"; brank="$3"; bfile="$4"
     (( wrank < 0 || brank < 0 )) && errexit "row not positive"
     (( wrank > 7 || brank > 7 )) && errexit "row not on board"
@@ -13,17 +13,16 @@ validate_board() {
 }
 
 queen_attack() {
-    [[ $1 == $3 || $1 != "-w"  || $3 != "-b" ]] &&
+    [[ $1 == "$3" || $1 != "-w"  || $3 != "-b" ]] &&
     errexit "Usage: ${0#*} -w <rank>,<file> -b <rank>,<file>"
 
     local -i wrank wfile brank bfile drank dfile
-    IFS=, read wrank wfile <<< "$2"
-    IFS=, read brank bfile <<< "$4"
+    IFS=, read -r wrank wfile <<< "$2"
+    IFS=, read -r brank bfile <<< "$4"
     validate_board $wrank $wfile $brank $bfile
-    drank=$(( $wrank-$brank < 0 ? $brank-$wrank : $wrank-$brank ))
-    dfile=$(( $wfile-$bfile < 0 ? $bfile-$wfile : $wfile-$bfile ))
-    (( drank * dfile == 0 || drank / dfile == 1 ))
-    [[ "$?" == 0 ]] && echo true || echo false
+    drank=$(( wrank-brank < 0 ? brank-wrank : wrank-brank ))
+    dfile=$(( wfile-bfile < 0 ? bfile-wfile : wfile-bfile ))
+    (( drank * dfile == 0 || drank / dfile == 1 )) && echo true || echo false
 }
 
 main() { queen_attack "$@" && exit 0; }
