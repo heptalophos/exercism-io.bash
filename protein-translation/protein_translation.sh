9e5a5972c34c3c4d83532cd85643b9533b454c84
Zero Delta <zerodeltahedge@gmail.com>
2024-10-30 22:54:04 +0200

    Create protein_translation.sh

---
+++ protein-translation/protein_translation.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+errexit() { echo "$*" >&2 && exit 1; }
+
+protein_translate() {
+    local rna="$1" codon protein
+    local -a proteins
+    local -rA codons_map=(
+        [AUG]=Methionine
+        [UUU]=Phenylalanine [UUC]=Phenylalanine
+        [UUA]=Leucine [UUG]=Leucine
+        [UCU]=Serine [UCC]=Serine [UCA]=Serine [UCG]=Serine
+        [UAU]=Tyrosine [UAC]=Tyrosine
+        [UGU]=Cysteine [UGC]=Cysteine
+        [UGG]=Tryptophan
+        [UAA]=STOP [UAG]=STOP [UGA]=STOP
+    )
+    while [[ -n $rna ]]
+    do
+        codon=${rna:0:3}
+        protein=${codons_map[$codon]}
+        [[ -n $protein ]] || errexit "Invalid codon"
+        [[ $protein == STOP ]] && break
+        proteins+=($protein)
+        rna=${rna:3}
+    done
+    printf '%s' "${proteins[*]}"
+}
+
+main() { protein_translate "$1" && exit 0; }
+
+set -e && main "$@"
