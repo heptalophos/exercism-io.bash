deafd8091b38826a690662d11d56b7825e7f4448
Zero Delta <zerodeltahedge@gmail.com>
2024-11-06 21:58:12 +0200

    Refactor sublist.sh

--- sublist/sublist.sh
+++ sublist/sublist.sh
@@ -3,18 +3,19 @@
 exitzero() { echo "$*" && exit 0;}
 
 main() {
-    set -o noglob
+    set -o noglob 
+    # shellcheck disable=2206
     local list1=(${1//[\[\],]/}) list2=(${2//[\[\],]/})
     local -i size1=${#list1[@]} size2=${#list2[@]}
     ((size1 > size2)) &&
     for ((i=0; i<=(size1-size2); i++)); do
-        [[ ${list1[@]:i:size2} == "${list2[@]}" ]] && exitzero superlist
+        [[ ${list1[*]:i:size2} == "${list2[*]}" ]] && exitzero superlist
     done
-    (( size1 < size2 )) &&
+    ((size1 < size2)) &&
     for ((i=0; i<=(size2-size1); i++)); do
-        [[ ${list2[@]:i:size1} == "${list1[@]}" ]] && exitzero sublist
-    done
-    [[ ${list1[@]} == "${list2[@]}" ]] && exitzero equal || exitzero unequal
+        [[ ${list2[*]:i:size1} == "${list1[*]}" ]] && exitzero sublist
+    done          
+    [[ ${list1[*]} == "${list2[*]}" ]] && exitzero equal || exitzero unequal
 }
 
 set -eu && main "$@" && exit 0
