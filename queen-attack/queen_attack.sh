284fa2a0c80497bbab919f0a4fd083d1364d228c
Zero Delta <zerodeltahedge@gmail.com>
2024-11-18 01:52:30 +0200

    Refactor queen_attack.sh

--- queen-attack/queen_attack.sh
+++ queen-attack/queen_attack.sh
@@ -16,13 +16,14 @@ queen_attack() {
     [[ $1 == "$3" || $1 != "-w"  || $3 != "-b" ]] &&
     errexit "Usage: ${0#*} -w <rank>,<file> -b <rank>,<file>"
 
-    local -i wrank wfile brank bfile drank dfile
+    local -i wrank wfile brank bfile drank dfile can_attack
     IFS=, read -r wrank wfile <<< "$2"
     IFS=, read -r brank bfile <<< "$4"
     validate_board $wrank $wfile $brank $bfile
     drank=$(( wrank-brank < 0 ? brank-wrank : wrank-brank ))
     dfile=$(( wfile-bfile < 0 ? bfile-wfile : wfile-bfile ))
-    (( drank * dfile == 0 || drank / dfile == 1 )) && echo true || echo false
+    can_attack=$(( drank * dfile == 0 || drank / dfile == 1 )) 
+    (( $can_attack )) && echo true || echo false
 }
 
 main() { queen_attack "$@" && exit 0; }
