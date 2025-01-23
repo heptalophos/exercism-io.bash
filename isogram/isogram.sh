5233ea5ecf53b4711d9485e8f883a6daf9018ebb
Zero Delta <zerodeltahedge@gmail.com>
2025-01-23 23:55:44 +0200

    Update isogram.sh

--- isogram/isogram.sh
+++ isogram/isogram.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
 
-boolx() { (( $? == 0 )) && echo '1' || echo '0'; }
-
 errexit() { echo "$*" >&2 && exit 1; }
 
+boolRC () { (( $? == 0 )) && echo true || echo false; }
+
 isogram() {
-    (( $# == 1 )) || errexit "Usage: "
-    local phrase="${1,,}" alpha="'a"
-    phrase=${phrase//[^a-z]/}
-    local -i charFlag=0
-    for (( i=0; i < ${#phrase}; i++ )); do
-        local char=${phrase:i:1}
-        if [[ $char = [a-z] ]]; then
-            local cp="'$char"
-            if (( (charFlag & (1 << (cp - alpha))) != 0 )); then
-                echo false && exit 0
-            fi
-            (( charFlag |= 1 << (cp - alpha) ))
-        fi
+    # (( $# == 1 )) || errexit "Usage: "
+    # local phrase="${1,,}" alpha=97
+    # phrase=${phrase//[^a-z]/}
+    # local -i charFlag=0
+    # for (( i=0; i < ${#phrase}; i++ )); do
+    #     local char=${phrase:i:1}
+    #     if [[ $char = [a-z] ]]; then 
+    #         local mask=$(( 1 << $char - $alpha ))
+    #         if (( (charFlag & 1 << (cp - $alpha)) != 0 )); then
+    #             echo false && exit 0
+    #         fi
+    #         (( charFlag |= 1 << ("$cp" - "$alpha") ))
+    #     fi
+    # done
+    # echo true
+    (( $# == 1 )) || exit 1
+    local -A found  # track discovered letters
+    local -i i  # loop counter
+    local word=${1,,}  # lowercased word
+    for (( i = 0; i < ${#word}; i++ )); do
+        chr="${word:$i:1}"
+        [[ $chr = [[:alpha:]] ]] || continue
+        [[ -v found[$chr] ]] && return 1 || found[$chr]=''
     done
-    echo true
 }
 
-main() { isogram $1 && exit 0; }
+main() { isogram $1; boolRC && exit 0; }
 
 set -e -u && main "$@"
