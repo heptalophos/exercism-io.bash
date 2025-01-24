cfd47dfb87376d981120186e24a11a59e2fa3bc9
Zero Delta <zerodeltahedge@gmail.com>
2025-01-24 17:47:24 +0200

    isogram.sh -- passing all

--- isogram/isogram.sh
+++ isogram/isogram.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
-boolx() { (( $? == 0 )) && echo '1' || echo '0'; }
-
 errexit() { echo "$*" >&2 && exit 1; }
 
+ord() { LC_CTYPE=C printf '%d' "'$1"; }
+
 isogram() {
-    (( $# == 1 )) || errexit "Usage: "
-    local phrase="${1,,}" alpha="'a"
-    phrase=${phrase//[^a-z]/}
-    local -i charFlag=0
-    for (( i=0; i < ${#phrase}; i++ )); do
-        local char=${phrase:i:1}
-        if [[ $char = [a-z] ]]; then
-            local cp="'$char"
-            if (( (charFlag & (1 << ("$cp" - "$alpha"))) != 0 )); then
-                echo false && exit 0
-            fi
-            (( charFlag |= 1 << ("$cp" - "$alpha") ))
-        fi
+    local -i alpha=$(ord a) flag=0
+    local phrase=${1,,}
+    for (( i=0; i < ${#phrase}; ++i )); do
+        char=${phrase:$i:1}
+        [[ ${char//[^a-z]} == "" ]] && continue
+        cp=$(ord "$char")
+        (( (flag & 1 << (cp - alpha)) )) && echo false && exit 0
+        (( flag |= 1 << (cp - alpha) ))
     done
     echo true
 }
 
-main() { isogram $1 && exit 0; }
-
-set -e -u && main "$@"
+main() {
+    [[ $# != 1 ]] && 
+    errexit "Usage: ${0#*} string_with_possible_spaces_or_punctuations"
+    isogram $@ && exit 0 
+}
+# set -x
+set -e && main "$@"
