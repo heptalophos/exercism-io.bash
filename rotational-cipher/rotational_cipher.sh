1beaf8aea03f0e98f01f26cd46c31d8631b236ec
Zero Delta <zerodeltahedge@gmail.com>
2025-02-14 23:29:34 +0200

    Update rotational_cipher.sh

--- rotational-cipher/rotational_cipher.sh
+++ rotational-cipher/rotational_cipher.sh
@@ -1,24 +1,53 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+alphabet_size=26
+lowercase_offset=97
+uppercase_offset=65
+
+is_lowercase () {
+    (( lowercase_offset <= ascii && ascii < lowercase_offset + alphabet_size ))
+}
+
+is_uppercase () {
+    (( uppercase_offset <= ascii && ascii < uppercase_offset + alphabet_size ))
+}
+
+rotate () {
+    local symbol="$1"
+    local ascii
+    ascii=$(printf '%d' "'$symbol")
+    local case_offset
+    if is_uppercase; then
+        case_offset=uppercase_offset
+    elif is_lowercase; then
+        case_offset=lowercase_offset
+    else
+        printf '%s' "$symbol"
+        return
+    fi
+    (( ascii+=offset ))
+    (( ascii-=case_offset ))
+    (( ascii%=alphabet_size ))
+    (( ascii+=case_offset ))
+    printf '%b' "$(printf '\\x%x' "$ascii")"
+}
+
+rot_cipher () {
+    local input="$1"
+    local offset="$2"
+    local cipher=''
+    for (( i=0; i<${#input}; i++ )); do
+        cipher+=$(rotate "${input:$i:1}")
+    done
+    echo "$cipher"
+}
+
+
+main() {
+    rot_cipher "$@" && exit 0
+}
+
+set -e && main $@
+
+
+
