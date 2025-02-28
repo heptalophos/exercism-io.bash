922ccf3f91953dec395980ba76e5a57ea7d6dee8
Zero Delta <zerodeltahedge@gmail.com>
2025-02-28 23:59:13 +0200

    Update rotational_cipher.sh

--- rotational-cipher/rotational_cipher.sh
+++ rotational-cipher/rotational_cipher.sh
@@ -1,53 +1,31 @@
 #!/usr/bin/env bash
 
-alphabet_size=26
-lowercase_offset=97
-uppercase_offset=65
-
-is_lowercase () {
-    (( lowercase_offset <= ascii && ascii < lowercase_offset + alphabet_size ))
-}
-
-is_uppercase () {
-    (( uppercase_offset <= ascii && ascii < uppercase_offset + alphabet_size ))
-}
-
-rotate () {
-    local symbol="$1"
-    local ascii
-    ascii=$(printf '%d' "'$symbol")
-    local case_offset
-    if is_uppercase; then
-        case_offset=uppercase_offset
-    elif is_lowercase; then
-        case_offset=lowercase_offset
-    else
-        printf '%s' "$symbol"
-        return
-    fi
-    (( ascii+=offset ))
-    (( ascii-=case_offset ))
-    (( ascii%=alphabet_size ))
-    (( ascii+=case_offset ))
-    printf '%b' "$(printf '\\x%x' "$ascii")"
-}
-
 rot_cipher () {
-    local input="$1"
-    local offset="$2"
-    local cipher=''
-    for (( i=0; i<${#input}; i++ )); do
-        cipher+=$(rotate "${input:$i:1}")
+    local plaintext="$1" cipher=""
+    local -i rot="$2" idx ridx
+    local -A map
+    while (( rot < 0 )); do (( rot += 26 )); done
+    (( rot %= 26 ))
+    local -ra alphabet=( {a..z} )
+    for (( idx = 0; idx < ${#alphabet[@]}; idx++ )); do
+        (( ridx = (idx + rot) % 26 ))
+        map[${alphabet[idx]}]="${alphabet[ridx]}"
+        map[${alphabet[idx]^^}]="${alphabet[ridx]^^}"
+    done
+    
+    for (( idx = 0; idx < ${#plaintext}; idx++ )); do
+        char=${plaintext:idx:1}
+        [[ -n ${map[$char]} ]] && char="${map[$char]}"
+        cipher+="$char"
     done
     echo "$cipher"
 }
-
 
 main() {
     rot_cipher "$@" && exit 0
 }
 
-set -e && main $@
+set -e && main "$@"
 
 
 
