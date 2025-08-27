2eea77b04d2bd5133285643734357d986861ab47
Zero Delta <zerodeltahedge@gmail.com>
2025-08-27 21:53:54 +0300

    sum-of-multiples solution

--- sum-of-multiples/sum_of_multiples.sh
+++ sum-of-multiples/sum_of_multiples.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+errexit() { echo "$*" >&2 && exit 1; }
+
+sum_of_multiples() {
+
+    local -a factors="${@:2}" multiples=()
+    local -i limit="$1" i=0 sum=0
+
+    (( $limit <= 0 )) && errexit "limit not positive"
+    (( ${#factors[@]} == 0 )) && errexit "no factors provided"
+
+    for f in ${factors}; do
+        if ((f != 0)); then
+            for ((i = f; i < limit; i += f)); do
+                multiples[$i]=1
+            done
+        fi
+    done
+
+    for factor in "${!multiples[@]}"; do
+        ((sum += factor))
+    done
+    echo "$sum"
+}
+
+main() {
+    sum_of_multiples "${1}" "${@:2}" && exit 0
+}
+
+set -e -u
+main "$@"
